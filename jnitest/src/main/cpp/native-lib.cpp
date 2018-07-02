eeaaec7a0571e9a39f16c6da5560f1bf9e5c55ed
likai <kai.l@verifone.cn>
2018-07-02 17:05:57 +0800

    添加native到java和从Java到native相互调用的例子

--- jnitest/src/main/cpp/native-lib.cpp
+++ jnitest/src/main/cpp/native-lib.cpp
@@ -3,17 +3,29 @@
 //
 #include <jni.h>
 #include <string>
+#include "debug.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+#define LOG_TAG "Native-lib"
+
+// 包名+类名+方法名(JNIEnv *env, jobject jobj)
 jstring
 Java_com_verifone_kail1_jnitest_MainActivity_stringFromJNI(JNIEnv *env, jobject jobj) {
     std::string hello = "hello world";
     char str[] = "Hello world";
     return env->NewStringUTF(str);
 }
+
+void
+Java_com_verifone_kail1_jnitest_MainActivity_stringToJNI(JNIEnv *env, jobject jobj, jstring jstr) {
+    const char *nativeString = env->GetStringUTFChars(jstr, false);
+    LOGD("%s\n", nativeString);
+    env->ReleaseStringUTFChars(jstr, nativeString);
+}
+
 
 #ifdef __cplusplus
 }
--- jnitest/src/main/java/com/verifone/kail1/jnitest/MainActivity.java
+++ jnitest/src/main/java/com/verifone/kail1/jnitest/MainActivity.java
@@ -3,6 +3,7 @@ package com.verifone.kail1.jnitest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,13 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonStringFromJni(View view) {
         Log.d("TAG", stringFromJNI());
     }
 
+    public void buttonStringToJni(View view) {
+        stringToJNI("Hello World From JAVA");
+    }
+
     public native String stringFromJNI();
+    public native void stringToJNI(String message);
 
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
     }
+
 }
